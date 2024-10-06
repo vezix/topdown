484e151f047fcf12666176f0b24b3815bad5caf4
Vezix Haikal <vezix123@gmail.com>
2024-10-06 16:12:55 +0800

    Add tileset
    ep 5 of the tutorial

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -16,8 +16,13 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	direction.x = Input.get_action_strength("right") - Input.get_action_strength("left")
-	direction.y = Input.get_action_strength("down") - Input.get_action_strength("up")
+	#direction.x = Input.get_action_strength("right") - Input.get_action_strength("left")
+	#direction.y = Input.get_action_strength("down") - Input.get_action_strength("up")
+	
+	direction = Vector2(
+		Input.get_axis("left", "right"),
+		Input.get_axis("up", "down")
+	).normalized()
 	pass
 
 func _physics_process(delta: float) -> void:
